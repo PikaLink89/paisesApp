e757b33bffb54fba5495ec397df57749c9d5d27a
Cristian Saball <c.saball@externo.ibermatica.com>
2021-05-06 22:42:33 +0200

    creando los modulos y componentes, exportandolos para que se puedan ver en todos los lados.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PaisModule } from './pais/pais.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PaisModule, //importamos los modulos que hemos creado para que Angular lo tenga en cuenta
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
