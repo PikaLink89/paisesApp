4fd2d6fae7d128699e3df68578154e4baf74a1cc
Cristian Saball <c.saball@externo.ibermatica.com>
2021-05-07 18:50:23 +0200

    Sistema de Rutas y usandolo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports: [ //los modulos siempre van en los imports
     BrowserModule,
+    AppRoutingModule,
     PaisModule, //importamos los modulos que hemos creado para que Angular lo tenga en cuenta
     SharedModule
   ],
